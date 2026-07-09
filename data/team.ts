ddc8bbf9b90b83044fff2082c63719235a57811e
Creylay <careyesla@gmail.com>
2026-07-09 10:23:22 -0400

    Update team and student avatars to use new directory structure

--- data/team.ts
+++ data/team.ts
@@ -8,42 +8,48 @@ export interface TeamMember {
 
 export const TEAM: TeamMember[] = [
   {
-    initials: 'FB', name: 'Felipe Bravo Márquez',
+    initials: 'FB', name: 'Felipe Bravo',
     roleKey: 'abt.team.felipe-bravo-marquez.role',
+    avatar: '/images/team/felipe_bravo.jpg',
   },
   {
     initials: 'CL', name: 'Claudia López',
     roleKey: 'abt.team.claudia-lopez.role',
-  },
-  {
-    initials: 'AC', name: 'Andrés Carvallo',
-    roleKey: 'abt.team.andres-carvallo.role',
+    avatar: '/images/team/claudia_lopez.jpg',
   },
   {
     initials: 'CT', name: 'Cristián Tamblay',
     roleKey: 'abt.team.cristian-tamblay.role',
+    avatar: '/images/team/cristian_tamblay.jpg',
   },
   {
     initials: 'CR', name: 'Camila Reyes',
     roleKey: 'abt.team.camila-reyes.role',
-    avatar: '/images/students/camila_reyes.jpg',
+    avatar: '/images/team/camila_reyes.jpg',
   },
   {
     initials: 'ED', name: 'Emilio Díaz',
     roleKey: 'abt.team.emilio-diaz.role',
-    avatar: '/images/students/emilio_diaz.png',
+    avatar: '/images/team/emilio_diaz.jpg',
   },
   {
     initials: 'FC', name: 'Felipe Cárdenas',
     roleKey: 'abt.team.felipe-cardenas.role',
-    avatar: '/images/students/felipe_cardenas.jpg',
+    avatar: '/images/team/felipe_cardenas.jpg',
   },
   {
     initials: 'NN', name: 'Nicolás Nast',
     roleKey: 'abt.team.nicolas-nast.role',
+    avatar: '/images/team/nicolas_nast.jpg',
+  },
+  {
+    initials: 'AC', name: 'Andrés Carvallo',
+    roleKey: 'abt.team.andres-carvallo.role',
+    avatar: '/images/team/andres_carvallo.jpg',
   },
   {
     initials: 'YH', name: 'Yelena Hernandez',
     roleKey: 'abt.team.yelena-hernandez.role',
+    avatar: '/images/team/yelena_hernandez.jpg',
   },
 ]
